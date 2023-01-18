9a679d1e71d407d9206f956678b305420bd786c5
Kira <kirakulikova255@gmail.com>
2023-01-18 23:14:28 +0300

    ex_21.txt

---
+++ ex_21.py
@@ -0,0 +1,17 @@
+line = 'фысабв, свфсфабв: аолым'
+
+while ',' in line or '.' in line or ';' in line:
+    line = line.replace(',', '')
+    line = line.replace('.', '')
+    line = line.replace(';', '')
+
+print(line)
+
+arr = line.split()
+print(arr)
+
+arr2 = []
+for word in arr:
+    if 'абв' not in word:
+        arr2.append(word)
+print(arr2)
